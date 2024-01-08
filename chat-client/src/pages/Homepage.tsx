7296cea07cde063969609cbe97ceb7471b35b4fe
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-08 15:28:54 +0530

    started working on login page

--- chat-client/src/pages/Homepage.tsx
+++ chat-client/src/pages/Homepage.tsx
@@ -1,5 +1,25 @@
+import { Container, Box, Text } from '@chakra-ui/react';
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Container maxW="xl" centerContent>
+        <Box
+          display="flex"
+          justifyContent="center"
+          p="3"
+          bg="white"
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Text> Chat App</Text>
+        </Box>
+        <Box></Box>
+      </Container>
+    </>
+  );
 };
 
 export default Homepage;
