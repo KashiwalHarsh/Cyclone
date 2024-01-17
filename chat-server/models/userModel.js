1fc005d4fe2bf5198e90d23a45e52c3da368d1d8
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-17 15:40:49 +0530

    worked more on the user formatting

--- chat-server/models/userModel.js
+++ chat-server/models/userModel.js
@@ -33,6 +33,7 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
+
 const User = new mongoose.model("User", userSchema);
 
 module.exports = User;
