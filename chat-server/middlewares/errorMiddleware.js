ed967b79598634f741c8f6830c2c627dcf06b50b
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-18 14:23:48 +0530

    added error handling middleware for 404 page

--- chat-server/middlewares/errorMiddleware.js
+++ chat-server/middlewares/errorMiddleware.js
@@ -1,4 +1,17 @@
-const notFound = () => { return 1 };
-const errorHandler = () => { return 2 };
+const notFound = (req, res, next) => {
+    const error = new Error(`404 : Not found - ${req.originalUrl}`)
+    res.status(404)
+    next(error);
+};
+
+//read about how to handle errors in nodejs
+const errorHandler = (req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? null : err.stack
+    })
+};
 
 module.exports = { notFound, errorHandler }
