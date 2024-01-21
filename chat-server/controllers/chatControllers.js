d9dc2d60e0f09c30fe8b81d552945c9d003b969c
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-21 23:23:29 +0530

    created the chat access and create Api

--- chat-server/controllers/chatControllers.js
+++ chat-server/controllers/chatControllers.js
@@ -1,8 +1,51 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const Chat = require("../models/chatModel");
+const User = require("../models/userModel");
 
 //creation of a new chat
+//understand by repeating it again
 const accessChat = asyncHandler(async (req, res) => {
+    const { userId } = req.body;
 
+    //if no userid is sent
+    if (!userId) {
+        console.log("UserId Param not sent with request")
+        return res.sendStatus(400)
+    }
+
+    //if the chat actually exist because of $and, if found populate the users and latestMessage
+    var isChat = await Chat.find({
+        isGroupChat: false,
+        $and: [
+            { users: { $elemMatch: { $eq: req.user._id } } },
+            { users: { $elemMatch: { $eq: userId } } }
+        ]
+    }).populate("users", "-password").populate("latestMessage")
+
+    isChat = await User.populate(isChat, {
+        path: "latestMessage.sender",
+        select: "name pic email"
+    })
+
+    if (isChat.length > 0) {
+        res.send(isChat[0])
+    } else {
+        var chatData = {
+            chatName: "sender",
+            isGroupChat: false,
+            users: [req.user._id, userId]
+        }
+
+        try {
+            const createdChat = await Chat.create(chatData)
+            const FullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "-password")
+
+            res.status(200).send(FullChat)
+        } catch (err) {
+            res.status(400)
+            throw new Error(err.message)
+        }
+    }
 })
 
 //fetch all the chats 
