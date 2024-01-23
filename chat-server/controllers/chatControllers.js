2f9322ab31f5d7573b8a78d1919fdba8dc037bc4
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-23 15:28:52 +0530

    added the group chat api

--- chat-server/controllers/chatControllers.js
+++ chat-server/controllers/chatControllers.js
@@ -71,24 +71,33 @@ const fetchChat = asyncHandler(async (req, res) => {
 
 //creating group chat
 const createGroupChat = asyncHandler(async (req, res) => {
-    const { userId } = req.body;
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please fill all the fields" });
+    }
+
+    var users = JSON.parse(req.body.users)
+    if (users.length < 2) {
+        return res.status(400).send({ message: "More than 2 users are required to form a group chat" })
+    }
+    users.push(req.user)
+
     try {
-        Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.user
+        })
+
+        const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
             .populate("users", "-password")
             .populate("groupAdmin", "-password")
-            .populate("latestMessage")
-            .sort({ updatedAt: -1 })
-            .then(async (results) => {
-                results = await User.populate(results, {
-                    path: "latestMessage.sender",
-                    select: "name pic email"
-                })
-                res.status(200).send(results)
-            })
+
+        res.status(200).json(fullGroupChat)
     } catch (err) {
         res.status(400)
         throw new Error(err.message)
     }
 })
 
-module.exports = { accessChat, fetchChat }
+module.exports = { accessChat, fetchChat, createGroupChat }
