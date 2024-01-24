1013cdd2c14734850ec484cd04af9fb04da1b559
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-24 16:44:26 +0530

    facing a lot of ts errors

--- chat-client/src/main.tsx
+++ chat-client/src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import ChatProvider from './context/ChatProvider.ts';
+import ChatProvider from './context/ChatProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChatProvider>
