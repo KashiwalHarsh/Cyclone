18a285dceb418b58bbe9189220712076a8eb0681
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-26 15:33:19 +0530

    working on search bar fontawesome icons

--- chat-client/src/components/chats/SideDrawer.tsx
+++ chat-client/src/components/chats/SideDrawer.tsx
@@ -1,5 +1,30 @@
+import { Box, Button, Text, Tooltip } from '@chakra-ui/react';
+
 const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip
+          label="Search user for Chat"
+          hasArrow
+          placeContent="bottom-end"
+        >
+          <Button variant="ghost">
+            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+            <Text>Search User</Text>
+          </Button>
+        </Tooltip>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
