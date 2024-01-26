d29f565557574dddeaed3d8ab4efe06ddb060e03
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-01-26 15:15:40 +0530

    started working on search box

--- chat-client/src/pages/Chats.tsx
+++ chat-client/src/pages/Chats.tsx
@@ -9,7 +9,13 @@ const Chats: React.FC = () => {
   return (
     <div style={{ width: '100%' }}>
       {user && <SideDrawer />}
-      <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
         {user && <MyChats />}
         {user && <ChatBox />}
       </Box>
