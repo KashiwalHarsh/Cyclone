f257361261bb81d51c724bdfd2f6dbd3449b1d36
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-02-24 18:16:50 +0530

    started working on chat box ui

--- chat-client/src/components/chats/MyChats.tsx
+++ chat-client/src/components/chats/MyChats.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { ChatState } from '../../context/ChatProvider';
-import { useToast } from '@chakra-ui/react';
+import { Box, Button, useToast } from '@chakra-ui/react';
 import axios from 'axios';
+import { AddIcon } from '@chakra-ui/icons';
 
 const MyChats = () => {
   const [loggedUser, setLoggedUser] = useState();
@@ -17,11 +18,11 @@ const MyChats = () => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-
       const { data } = await axios.get(
         'http://localhost:3000/api/chats',
         config
       );
+      console.log(data);
       setChats(data);
     } catch (error) {
       toast({
@@ -39,7 +40,38 @@ const MyChats = () => {
     setLoggedUser(JSON.parse(localStorage.getItem('userInfo')));
     fetchChats();
   }, []);
-  return <>MyChats</>;
+  return (
+    <Box
+      display={{ base: selectedChat ? 'none' : 'flex', md: 'flex' }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: '100%', md: '31%' }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: '18px', md: '20px' }}
+        fontFamily="Work sans"
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Chats
+        <Button
+          display="flex"
+          fontSize={{ base: '15px', md: '10px', lg: '15px' }}
+          rightIcon={<AddIcon />}
+        >
+          New Group Chat
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default MyChats;
