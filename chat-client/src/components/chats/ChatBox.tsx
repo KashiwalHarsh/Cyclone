3ab4d13c806fe3a81cebf6f7f9ecafe2be6c2910
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2024-02-28 23:58:19 +0530

    made the single chat page option

--- chat-client/src/components/chats/ChatBox.tsx
+++ chat-client/src/components/chats/ChatBox.tsx
@@ -1,5 +1,22 @@
+import { Box } from '@chakra-ui/react';
+import { ChatState } from '../../context/ChatProvider';
+
 const ChatBox = () => {
-  return <div>ChatBox</div>;
+  const { selectedChat } = ChatState();
+  return (
+    <Box
+      display={{ base: selectedChat ? 'flex' : 'none', md: 'flex' }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: '100%', md: '68%' }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      Single Chat
+    </Box>
+  );
 };
 
 export default ChatBox;
